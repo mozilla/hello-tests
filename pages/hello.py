9623621c2b2d0f3409aba5c678a6d2b68f79e9aa
Dave Hunt <dave.hunt@gmail.com>
2016-06-09 10:33:01 +0100

    Migrate to PyPOM

--- pages/hello.py
+++ pages/hello.py
@@ -2,34 +2,25 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import uuid
-
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait as Wait
 
-from pages.page import Page
+from pages.base import Base
 
 
-class HelloPage(Page):
+class Hello(Base):
 
     _hello_logo_locator = (By.CSS_SELECTOR, '.hello-logo')
     _failed_room_message_locator = (By.CSS_SELECTOR, '.failed-room-message')
 
-    @property
-    def _url(self):
-        return '{base_url}/{id}'.format(base_url=self.base_url, id=uuid.uuid4())
-
     def wait_for_page_to_load(self):
-        body = self.selenium.find_element(By.TAG_NAME, 'body')
-        Wait(self.selenium, self.timeout).until(
-            lambda s: 'is-standalone-room' in body.get_attribute('class'))
+        body = self.find_element(By.TAG_NAME, 'body')
+        self.wait.until(lambda s: 'is-standalone-room' in body.get_attribute('class'))
         return self
 
     @property
     def failed_room_message(self):
-        return self.selenium.find_element(
-            *self._failed_room_message_locator).text
+        return self.find_element(*self._failed_room_message_locator).text
 
     @property
     def is_hello_logo_visible(self):
-        return self.is_element_visible(self._hello_logo_locator)
+        return self.is_element_displayed(*self._hello_logo_locator)
